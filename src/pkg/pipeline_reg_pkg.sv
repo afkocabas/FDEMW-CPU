8bac90c0c2e890fbd62cfbcdd271120e463cd951
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-07 21:44:16 +0300

    pipeline registers' definitions added.

---
+++ src/pkg/pipeline_reg_pkg.sv
@@ -0,0 +1,11 @@
+package pipeline_reg_pkg;
+  import fdemw_pkg::*;
+  import riscv32i_pkg::*;
+
+  typedef struct packed {
+    logic  valid;
+    inst_t inst;
+    addr_t pc;
+  } if_id_reg_t;
+
+endpackage
