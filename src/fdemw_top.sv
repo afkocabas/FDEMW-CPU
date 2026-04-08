de913b37c68df5ac91b8b8408dd560ac94805573
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-08 22:39:12 +0300

    fdemw_top.sv and constrain.xdc modified.

--- src/fdemw_top.sv
+++ src/fdemw_top.sv
@@ -1,18 +1,23 @@
 import fdemw_pkg::*;
 import riscv32i_pkg::*;
+import pipeline_reg_pkg::*;
 
 module fdemw_top (
     input logic clk_i,
     input logic res_i,
+
+
+    input logic stall_i,
 
     output seg_t seg_o,
     output logic [3:0] an_o
 );
 
   // Internal signals (wires of other modules)
-  addr_t fetch_addr;
-  inst_t i_mem_o;
   inst_kind_e dec_inst_kind;
+
+  if_id_reg_t if_id_reg;
+  imem_if imem_iff ();
 
   // Internal signals
   logic [3:0] an_o_c;
@@ -21,7 +26,6 @@ module fdemw_top (
   always_comb begin : assign_comb
     an_o_c = 4'b1110;
   end
-
 
   always_comb begin : seg_comb
     unique case (dec_inst_kind)
@@ -41,22 +45,23 @@ module fdemw_top (
       .clk_i(clk_i),
       .res_i(res_i),
 
-      .addr_o(fetch_addr)
+      .imem_if(imem_iff.fetch),
+      .if_id_o(if_id_reg),
+      .stall_i(stall_i)
   );
 
   decode decode_core (
-      .clk_i (clk_i),
-      .res_i (res_i),
-      .inst_i(i_mem_o),
+      .clk_i(clk_i),
+      .res_i(res_i),
 
+      .if_id_reg_i(if_id_reg),
       .inst_kind_o(dec_inst_kind)
   );
 
   inst_mem i_mem (
-      .clk_i (clk_i),
-      .addr_i(fetch_addr),
+      .clk_i(clk_i),
 
-      .inst_o(i_mem_o)
+      .fetch_if(imem_iff.imem)
   );
 
   assign an_o  = an_o_c;
