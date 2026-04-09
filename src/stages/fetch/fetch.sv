a13be7a8519ce98dfd57c1e081f9563ae7d71170
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-09 12:46:21 +0300

    Fetch stage FSM implemented.

--- src/stages/fetch/fetch.sv
+++ src/stages/fetch/fetch.sv
@@ -14,11 +14,16 @@ module fetch (
     imem_if.fetch imem_if
 );
 
+  typedef enum {
+    IDLE,
+    WAIT_RESP
+  } f_state_t;
+
   /* State Registers and Next State Signals*/
+  f_state_t state_q, state_d;
   addr_t prg_cnt_q, prg_cnt_d;
   if_id_reg_t if_id_reg_q, if_id_reg_d;
   logic [26:0] cnt_q, cnt_d;  // Clock divider
-  logic has_active_req_q, has_active_req_d;
 
 
   /* Internal signals */
@@ -26,43 +31,58 @@ module fetch (
   logic  req_valid_c;
   addr_t inst_addr_c;
 
-  always_comb begin
+  always_comb begin : derived_macros
     is_cnt_done = (cnt_q == '1);
   end
 
   always_comb begin : fsm_block
+    // Default state register assingments
+    state_d = state_q;
     prg_cnt_d = prg_cnt_q;
     if_id_reg_d = if_id_reg_q;
-    has_active_req_d = has_active_req_q;
 
+    // Default output signal assignments
     inst_addr_c = prg_cnt_q;
-    req_valid_c = has_active_req_q;
+    req_valid_c = LOW;
 
-    // Flush has the priority
+    // Flush has should should be immediate.
     if (flush_i) begin
-      prg_cnt_d = '0;
+      prg_cnt_d = flush_addr_i;
       if_id_reg_d = '0;
-      has_active_req_d = LOW;
-
-      inst_addr_c = prg_cnt_q;
-    end else
-    if (stall_i) begin
-
+      state_d = IDLE;
     end else begin
-      if (!has_active_req_q) begin
-        has_active_req_d = HIGH;
-      end
-      if (has_active_req_q && imem_if.resp_valid) begin
-        if (is_cnt_done) begin
-
-          prg_cnt_d = prg_cnt_q + 4;
-
-          if_id_reg_d.inst = imem_if.inst;
-          if_id_reg_d.pc = prg_cnt_q;
-          if_id_reg_d.valid = HIGH;
-
+      unique case (state_q)
+        IDLE: begin
+          if (!stall_i) begin
+            req_valid_c = HIGH;
+            inst_addr_c = prg_cnt_q;
+            state_d = WAIT_RESP;
+          end
         end
-      end
+        WAIT_RESP: begin
+          req_valid_c = HIGH;
+          if (imem_if.resp_valid && is_cnt_done) begin
+            if (!stall_i) begin
+              prg_cnt_d = prg_cnt_q + 4;
+
+              if_id_reg_d.inst = imem_if.inst;
+              if_id_reg_d.pc = prg_cnt_q;
+              if_id_reg_d.valid = HIGH;
+
+              // WARN: The following line incurs a bubble in the pipeline.
+              // The reason is the following:
+              // After state transition, IDLE does not check memory response in the same cycle, it only issues the transition to WAIT_RESP.
+              // Only after then, memory repsonse is checked.
+
+              // state_d = IDLE;
+
+            end else begin
+              // TODO: If the memory responds but there is stall, buffer the
+              // instruction or leave it here.
+            end
+          end
+        end
+      endcase
     end
   end
 
@@ -81,13 +101,13 @@ module fetch (
       cnt_q <= '0;
       prg_cnt_q <= '0;
       if_id_reg_q <= '0;
-      has_active_req_q <= '0;
+      state_q <= IDLE;
     end else begin
       cnt_q <= cnt_d;
       if (is_cnt_done) begin
         prg_cnt_q <= prg_cnt_d;
         if_id_reg_q <= if_id_reg_d;
-        has_active_req_q <= has_active_req_d;
+        state_q <= state_d;
       end
     end
 
