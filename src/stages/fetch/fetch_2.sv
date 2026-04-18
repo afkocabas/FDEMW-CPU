68cf1d1c2c0136ca7db80f96b081c1a1ef441785
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-18 12:43:16 +0300

    FSMless fetch module implemented.

--- src/stages/fetch/fetch_2.sv
+++ src/stages/fetch/fetch_2.sv
@@ -2,46 +2,78 @@ import fdemw_pkg::*;
 import riscv32i_pkg::*;
 
 module fetch_2 (
-    input logic  clk_i,
-    input logic  res_i,
-    input logic  fetch_en_i,
-    input addr_t pc_i,
+    input logic res_i,
+    input logic clk_i,
 
-    output if_id_reg_t if_id_reg_o,
+    input logic stall_i,
 
+    input logic  flush_i,
+    input addr_t flush_pc_i,
+
+    output if_id_reg_t if_id_o,
     imem_if.fetch imem_if
 );
 
+  addr_t pc_q, pc_d;
   addr_t pc_req_q, pc_req_d;
+  logic req_flight_q, req_flight_d;
+  logic drop_req_q, drop_req_d;
 
   always_comb begin : fetch_comb
+    pc_d = pc_q;
     pc_req_d = pc_req_q;
+    req_flight_d = LOW;
+    drop_req_d = LOW;
 
-    if_id_reg_o.pc = '0;
-    if_id_reg_o.inst = '0;
-    if_id_reg_o.valid = '0;
+    if_id_o.pc = '0;
+    if_id_o.inst = '0;
+    if_id_o.valid = '0;
 
     imem_if.inst_addr = '0;
     imem_if.req_valid = LOW;
 
+    if (res_i) begin
+      imem_if.inst_addr = '0;
+      imem_if.req_valid = LOW;
+    end else if (flush_i) begin
+      pc_d = flush_pc_i;
+      drop_req_d = HIGH;
+    end else if (stall_i) begin
+      if (req_flight_q) begin
 
-    if (imem_if.resp_valid) begin
-      if_id_reg_o.pc = pc_req_q;
-      if_id_reg_o.inst = imem_if.inst;
-      if_id_reg_o.valid = HIGH;
+      end
+    end else begin
+      req_flight_d = HIGH;
+      if (imem_if.resp_valid && !drop_req_q) begin
+        if_id_o.pc = pc_req_q;
+        if_id_o.inst = imem_if.inst;
+        if_id_o.valid = HIGH;
+
+        pc_d = pc_q + 4;
+        pc_req_d = pc_d;
+
+        imem_if.inst_addr = pc_d;
+        imem_if.req_valid = HIGH;
+      end else begin
+        pc_req_d = pc_q;
+        imem_if.inst_addr = pc_q;
+        imem_if.req_valid = HIGH;
+      end
     end
-
-    if (fetch_en_i) begin
-      pc_req_d = pc_i;
-      imem_if.inst_addr = pc_i;
-      imem_if.req_valid = HIGH;
-    end
-
   end
 
   always_ff @(posedge clk_i) begin : blockName
-    if (res_i) pc_req_q <= '0;
-    else pc_req_q <= pc_req_d;
+    if (res_i) begin
+      pc_req_q <= '0;
+      pc_q <= '0;
+      req_flight_q <= LOW;
+      drop_req_q <= LOW;
+    end else begin
+      pc_req_q <= pc_req_d;
+      pc_q <= pc_d;
+      req_flight_q <= req_flight_d;
+      drop_req_q <= drop_req_d;
+    end
   end
 
 endmodule
