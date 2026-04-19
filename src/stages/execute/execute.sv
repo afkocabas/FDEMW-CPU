d8101583c952a37d32e0a7e39cb510d2fdf29bb0
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-19 13:33:04 +0300

    Execute state bug fix and alignment for laod-use data hazard.

--- src/stages/execute/execute.sv
+++ src/stages/execute/execute.sv
@@ -18,38 +18,25 @@ module execute (
     op_a = '0;
     op_b = '0;
 
-    unique case (forward_sel_a_i)
-      NO_FORWARD: begin
-        unique case (id_exe_reg_i.alu_src1)
-          SRC1_RS1:  op_a = id_exe_reg_i.rs1_data;
-          SRC1_IMM:  op_a = id_exe_reg_i.imm;
-          SRC1_ZERO: op_a = '0;
-          SRC1_PC:   op_a = id_exe_reg_i.pc;
-          default:   op_a = '0;
-        endcase
+    // Driving Operand A and Operand B
+    unique case (id_exe_reg_i.alu_src1)
+      SRC1_RS1: begin
+        if (forward_sel_a_i == FORWARD_FROM_EXE) op_a = exe_forward_op_i;
+        else if (forward_sel_a_i == FORWARD_FROM_MEM) op_a = mem_forward_op_i;
+        else op_a = id_exe_reg_i.rs1_data;
       end
-      FORWARD_FROM_EXE: begin
-        op_a = exe_forward_op_i;
-      end
-      FORWARD_FROM_MEM: begin
-        op_a = mem_forward_op_i;
-      end
+      SRC1_IMM:  op_a = id_exe_reg_i.imm;
+      SRC1_ZERO: op_a = '0;
+      SRC1_PC:   op_a = id_exe_reg_i.pc;
     endcase
 
-    unique case (forward_sel_b_i)
-      NO_FORWARD: begin
-        unique case (id_exe_reg_i.alu_src2)
-          SRC2_RS2: op_b = id_exe_reg_i.rs2_data;
-          SRC2_IMM: op_b = id_exe_reg_i.imm;
-          default:  op_b = '0;
-        endcase
+    unique case (id_exe_reg_i.alu_src2)
+      SRC2_RS2: begin
+        if (forward_sel_b_i == FORWARD_FROM_EXE) op_b = exe_forward_op_i;
+        else if (forward_sel_b_i == FORWARD_FROM_MEM) op_b = mem_forward_op_i;
+        else op_b = id_exe_reg_i.rs2_data;
       end
-      FORWARD_FROM_EXE: begin
-        op_b = exe_forward_op_i;
-      end
-      FORWARD_FROM_MEM: begin
-        op_b = mem_forward_op_i;
-      end
+      SRC2_IMM: op_b = id_exe_reg_i.imm;
     endcase
 
   end
@@ -63,6 +50,17 @@ module execute (
 
   always_comb begin : output_comb
     exe_mem_reg_o.id_exe_reg = id_exe_reg_i;
+
+    if (forward_sel_a_i == FORWARD_FROM_EXE) exe_mem_reg_o.id_exe_reg.rs1_data = exe_forward_op_i;
+    else
+    if (forward_sel_a_i == FORWARD_FROM_MEM);
+    else exe_mem_reg_o.id_exe_reg.rs1_data = mem_forward_op_i;
+
+    if (forward_sel_b_i == FORWARD_FROM_EXE) exe_mem_reg_o.id_exe_reg.rs2_data = exe_forward_op_i;
+    else
+    if (forward_sel_b_i == FORWARD_FROM_MEM);
+    else exe_mem_reg_o.id_exe_reg.rs2_data = mem_forward_op_i;
+
     exe_mem_reg_o.alu_result = alu_result;
   end
 
