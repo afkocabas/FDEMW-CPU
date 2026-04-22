bf8a9563e170f75870b51f8ebc64cb95aa90bab7
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-04-22 22:34:01 +0300

    Control unit created.

---
+++ src/stages/control_unit.sv
@@ -0,0 +1,14 @@
+module control_unit (
+    input logic memory_stall_i,
+    input logic branch_taken_i,
+
+    output logic pipeline_stall_o,
+    output logic flush_frontend_o
+
+);
+
+  // TODO: Complete the logic here
+  always_comb begin : output_comb
+
+  end
+endmodule
